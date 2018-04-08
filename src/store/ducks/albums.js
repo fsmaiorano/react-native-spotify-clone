e029904d7002393db55f28751755ea2801922f2d
Fábio <fsmaiorano@gmail.com>
2018-04-08 17:28:43 -0300

    add: seamless-immutable

--- src/store/ducks/albums.js
+++ src/store/ducks/albums.js
@@ -1,14 +1,16 @@
+import Immutable from 'seamless-immutable';
+
 export const Types = {
   GET_REQUEST: 'albums/GET_REQUEST',
   GET_SUCCESS: 'albums/GET_SUCCESS',
   GET_FAILURE: 'albums/GET_FAILURE',
 };
 
-const initialState = {
+const initialState = Immutable({
   data: [],
   loading: false,
   error: null,
-};
+});
 
 export default function albums(state = initialState, action) {
   switch (action.type) {
